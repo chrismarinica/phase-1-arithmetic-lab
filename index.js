d59ae02b4b875294dc4a32cb6c6554e4af9659bc
Christopher Marinica <chris.marinica@gmail.com>
2022-10-31 01:05:44 -0400

    commit tree 8532c4f8cc67189d3960357415fb02ebde90d8d1

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let multiply = 62;
+let num1 = 31
+let num2 = 2
 
-// Write your code here
+let random = Number.isInteger(Math.random() * 10) + 1;
+
+let mod = 4;
+let num3 = 24;
+let num4 = 8;
+
+let max = Math.max(1,5,10,20);
